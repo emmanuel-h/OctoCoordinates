d51b360cff817930d1ba87e7c6eb21df8c433e12
emmanuel-h <tchezmoi@gmail.com>
2019-09-18 15:11:55 +0200

    Fully debug LUA script

--- OctoCoordinates.lua
+++ OctoCoordinates.lua
@@ -1,27 +1,29 @@
 local zone
 local TimeSinceLastUpdate = 0
-local function UpdateCoordinates(elapsed)
+local function UpdateCoordinates(self, elapsed)
     if zone ~= GetRealZoneText() then
         zone = GetRealZoneText()
-        SetMapToCurrentZone ()
     end
     TimeSinceLastUpdate = TimeSinceLastUpdate + elapsed
     if TimeSinceLastUpdate > .5 then
         TimeSinceLastUpdate = 0
-        local posX, posY = GetPlayerMapPosition("player")
-        local x = math.floor(posX * 10000)/100
-        local y = math.floor(posY * 10000)/100
+        local map = C_Map.GetBestMapForUnit("player")
+        local position = C_Map.GetPlayerMapPosition(map, "player")
+        local x = math.floor(position.x * 10000)/100
+        local y = math.floor(position.y * 10000)/100
         OctoCoordinatesFontString:SetText("|c98FB98ff("..x..", "..y..")")
     end
-    end
+end
 
-function OctoCoordinates_OnLoad(self)
+function OctoCoordinates_OnLoad(self, event, ...)
     self:RegisterEvent("ADDON_LOADED")
 end
 function OctoCoordinates_OnEvent(self, event, ...)
-    if event == "ADDON_LOADED" and ... == "OctoCoordinates" then
-        self:UnregisteredEvent("ADDON_LOADED")
+    if (event == "ADDON_LOADED" and ... == "OctoCoordinates") then
+        print("OctoCoordinates loaded")
+        self:UnregisterEvent("ADDON_LOADED")
         OctoCoordinates:SetSize(100, 50)
+        print("test")
         OctoCoordinates:SetPoint("TOP", "Minimap", "BOTTOM", 5, -5)
         OctoCoordinates:SetScript("OnUpdate", UpdateCoordinates)
         local coordsFont = OctoCoordinates:CreateFontString("OctoCoordinatesFontString", "ARTWORK", "GameFontNormal")
